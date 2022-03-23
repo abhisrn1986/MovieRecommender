763266b166ce09d19bc111d3ea325757048f6549
Abhishek Srinivas <abhisrn@gmail.com>
2022-03-23 09:50:37 +0100

    [RecommenderSystem] Minor refactoring and removing some comments

--- RecommenderSystems/streamlit_app.py
+++ RecommenderSystems/streamlit_app.py
@@ -1,15 +1,15 @@
+import imdb
 import matplotlib.pyplot as plt
 import pandas as pd
 import plotly.express as px
 import seaborn as sns
 import streamlit as st
-import imdb
 
 # create an instance of the Cinemagoer class
 cinemagoer = imdb.Cinemagoer()
 
 from recommender import recommend_most_popular, recommend_nmf, recommend_random
-from utils import create_R_matrix, create_model, movies
+from utils import create_model, create_R_matrix, movies
 
 imdb_logo_url = 'https://ia.media-imdb.com/images/M/MV5BMTczNjM0NDY0Ml5BMl5BcG5nXkFtZTgwMTk1MzQ2OTE@._V1_.png'
 
@@ -32,57 +32,65 @@ def create_links_markdown(imdb_objs) :
     list_imdb_links = list_imdb_links[:-2]
     return list_imdb_links
 
-movies_list=movies['title'].tolist()
+if __name__ == '__main__':
 
-# form for adding movies
-main_form = st.form(key='my-form')
-recommender_type = main_form.selectbox(
-     'Movie Recommender Type',
-     ('NMF', 'MostPopular', 'Random'))
-n_movie_recommendations = main_form.number_input('Number of Recommendations', value = 5)
-selected_movie = main_form.selectbox('Movie', tuple(movies_list))
-movie_rating = main_form.slider(label='Rating', min_value=1, max_value=5, key=4)
+    movies_list=movies['title'].tolist()
 
-add_more_movies = main_form.form_submit_button('Add Movie')
-delete_movies = main_form.form_submit_button('Clear Movies')
-submit = main_form.form_submit_button('Recommend Movies')
+    # form for adding movies
+    main_form = st.form(key='my-form')
+    recommender_type = main_form.selectbox(
+        'Movie Recommender Type',
+        ('NMF', 'MostPopular', 'Random'))
+    n_movie_recommendations = main_form.number_input('Number of Recommendations', value = 5)
+    selected_movie = main_form.selectbox('Movie', tuple(movies_list))
+    movie_rating = main_form.slider(label='Rating', min_value=1, max_value=5, key=4)
 
-current_movies_heading = st.empty()
-current_movies_heading.markdown("## Current User Movie Ratings:")
-current_movies = st.empty()
-recommended_movies = st.empty()
+    add_more_movies = main_form.form_submit_button('Add Movie')
+    delete_movies = main_form.form_submit_button('Clear Movies')
+    submit = main_form.form_submit_button('Recommend Movies')
 
-if add_more_movies :
-    get_movies()[selected_movie] = movie_rating
+    current_movies_heading = st.empty()
+    current_movies_heading.markdown("## Current User Movie Ratings:")
     current_movies = st.empty()
-    current_movies.table(get_movies_df())
+    recommended_movies = st.empty()
 
-if delete_movies :
-    current_movies = st.empty()
-    get_movies().clear()
-    # current_movies.write(f"Current added movies:  \n  {get_movies()}")
-    current_movies.table(get_movies_df())
-    
+    if add_more_movies :
+        get_movies()[selected_movie] = movie_rating
+        current_movies = st.empty()
+        current_movies.table(get_movies_df())
 
-if submit:
+    if delete_movies :
+        current_movies = st.empty()
+        get_movies().clear()
+        current_movies.table(get_movies_df())
+        
 
-    user_rating = get_movies()
-    recommended_movie_list = ""
+    if submit:
 
-    if len(user_rating.values()) != 0:
+        current_movies.table(get_movies_df())
+
+        user_rating = get_movies()
+        recommended_movie_list = ""
+
         if(recommender_type == 'Random'):
             recs = recommend_random(k = n_movie_recommendations)
         elif(recommender_type == 'MostPopular'):
             recs = recommend_most_popular(k = n_movie_recommendations)
         elif(recommender_type == 'NMF'):
-            R = create_R_matrix()
-            model = create_model(R, n_components=55, overwrite=False)
-            recs = recommend_nmf(user_rating, model, R.shape[1], k = n_movie_recommendations)
+            if len(user_rating.values()) != 0:
+                R = create_R_matrix()
+                model = create_model(R, n_components=55, overwrite=False)
+                recs = recommend_nmf(user_rating, model, R.shape[1], k = n_movie_recommendations)
+            else:
+                recs = []
         else:
             recs = recommend_most_popular(n_movie_recommendations)
 
         
         recommended_movies.markdown(f"""## Recommended Movies Using {recommender_type}: """)
+
+        if len(recs) == 0:
+            st.markdown(f"No movies added. Add movies for {recommender_type} recommender to work on something")
 
         for movie_index, movie in enumerate(recs):
 
@@ -110,7 +118,7 @@ if submit:
             st.markdown(f"  \n  {movie_index + 1}. [{movie}]({movie_url})  \n  ")
             st.markdown(f'''
             <a href="{movie_url}">
-                <img src="{movie_cover_url}" width="100" height="150"/>
+                <img src="{movie_cover_url}" width="150" height="225"/>
             </a>''',
             unsafe_allow_html=True
             )
@@ -121,8 +129,5 @@ if submit:
 
             st.markdown("#")
             st.markdown("#")
-      
-
-    current_movies.table(get_movies_df())
 
 
